5d67d646ef4e8edf471ccbcbeffcc3e495bf13a9
Leonardo Bolivar <bolivar.soret@gmail.com>
2019-04-15 16:02:02 -0400

    remote method test InvAgent

--- common/models/InvAgent.js
+++ common/models/InvAgent.js
@@ -2,19 +2,49 @@
 // Node module: loopback-example-access-control
 // This file is licensed under the Artistic License 2.0.
 // License text available at https://opensource.org/licenses/Artistic-2.0
+'use strict';
+
+const myPool = require ('../../connectors/pool_cc_reports');
+
+const poolDat = myPool.poolCCreports;
 
 module.exports = function(InvAgent) {
 
-  /**
- * gets a list of agents based on an array of params
- * @param {object} arg array of parameters
- * @param {Function(Error, array)} callback
- */
+  InvAgent.greet = async function(msg) {
+    return sql(msg);
+  }
 
-  InvAgent.InvAgent_list = function(arg, callback) {
-    var list;
-    // TODO
-    callback(null, list);
-  };
+  InvAgent.remoteMethod('greet', {
+        accepts: {arg: 'msg', type: 'array', http: { source: 'body'} },
+        returns: {arg: 'result', type: 'array'}
+  });
+
+  async function sql(msg){
+    
+    console.log("MSG", msg);
+    let filter = msg[0].status;  
+    console.log("FILTER", filter); 
+    let querySQL_cdr = 
+
+        `
+        SELECT * 
+        FROM inv_agentes
+        WHERE estatus_inv_agentes = ${filter} 
+        `;
+    
+
+        try {
+        
+        var result = await poolDat.query(querySQL_cdr);
+        // var result_cdr = JSON.stringify(result);
+        //console.log('****** CDR ******');
+        //console.log(result);
+        // console.log(querySQL_cdr);
+        return result;
+            
+        } catch (error) {
+            
+        }
+  }
 
 };

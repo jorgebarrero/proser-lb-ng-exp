4815650941362a3192ef6c30db5afb57b843b211
Leonardo Bolivar <bolivar.soret@gmail.com>
2019-04-17 14:04:55 -0400

    InvReport with remote method

--- common/models/InvAgent.js
+++ common/models/InvAgent.js
@@ -4,42 +4,6 @@
 // License text available at https://opensource.org/licenses/Artistic-2.0
 'use strict';
 
-const myPool = require ('../../connectors/pool');
-const constructor = require ('../../helpers/constructor');
-
-const poolDat = myPool.poolDat;
-
 module.exports = function(InvAgent) {
-
-  InvAgent.mainQuery = async function(filtro) {
-    return sql(filtro);
-  }
-
-  InvAgent.remoteMethod('mainQuery', {
-        accepts: {arg: 'filtro', type: 'object', http: { source: 'body'} },
-        returns: {arg: 'result', type: 'array'}
-  });
-
-  async function sql(filtro){
-    
-    console.log("FILTRO", filtro);
-    var querySQL = await constructor.mainQuery(filtro);
-    console.log("QUERY", querySQL);
-    
-
-        try {
-        
-        var result = await poolDat.query(querySQL);
-        console.log(result);
-        return result;
-            
-        } catch (error) {
-
-          console.log('Server error');
-          console.log(querySQL);
-          res.status(500).send('Server error');
-            
-        }
-  }
 
 };
---
+++ common/models/InvReport.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const myPool = require ('../../connectors/pool');
+const constructor = require ('../../helpers/constructor');
+
+const poolDat = myPool.poolDat;
+
+module.exports = function(Invreport) {
+
+    Invreport.mainQuery = async function(filtro) {
+        return sql(filtro);
+      }
+    
+      Invreport.remoteMethod('mainQuery', {
+            accepts: {arg: 'filtro', type: 'object', http: { source: 'body'} },
+            returns: {arg: 'result', type: 'array'}
+      });
+    
+      async function sql(filtro){
+        
+        console.log("FILTRO", filtro);
+        var querySQL = await constructor.mainQuery(filtro);
+        console.log("QUERY", querySQL);
+        
+    
+            try {
+            
+            var result = await poolDat.query(querySQL);
+            console.log(result);
+            return result;
+                
+            } catch (error) {
+    
+              console.log('Server error');
+              console.log(querySQL);
+              res.status(500).send('Server error');
+                
+            }
+      }
+
+};

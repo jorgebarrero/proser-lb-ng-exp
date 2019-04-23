4de5eb2cb818df13597594f803bcffd4887b1538
Leonardo Bolivar <bolivar.soret@gmail.com>
2019-04-23 13:33:04 -0400

    acl first part

--- src/common/query/dashboard.js
+++ src/common/query/dashboard.js
@@ -11,18 +11,47 @@ SUM(duration)/SUM(cdr_call_received),
 sum(cdr_qlog_secs_at_wait)/SUM(cdr_call_received)
 `;
 
-let SQLAudit = `
-COUNT()
-`
-SELECT 'LOGIN', COUNT(id_break), MIN(datetime_init), MAX(datetime_end) FROM `MainAudit` WHERE id_break = '0'
+// let SQLAudit = `
+// COUNT()
+// `
+// SELECT 
+// 'LOGIN', 
+// COUNT(id_break), 
+// MIN(datetime_init), 
+// MAX(datetime_end) 
 
-UNION
+// FROM `MainAudit` 
 
-SELECT 'AUXILIAR', COUNT(audit.id_break) FROM `MainAudit` as audit JOIN `InvBreak` as break ON audit.id_break = break.inv_break_id  WHERE audit.id_break <> '0' AND break.inv_break_productivity = '0'
+// WHERE id_break = '0'
 
-UNION
+// UNION
 
-SELECT 'ASIGNATION', COUNT(audit.id_break) FROM `MainAudit` as audit JOIN `InvBreak` as break ON audit.id_break = break.inv_break_id  WHERE audit.id_break <> '0' AND break.inv_break_productivity = '1'
+// SELECT 
+// 'AUXILIAR', 
+// COUNT(audit.id_break) 
+// FROM `MainAudit` as audit JOIN `InvBreak` as break 
+// ON 
+// audit.id_break = break.inv_break_id  
+
+// WHERE 
+// audit.id_break <> '0' 
+// AND 
+// break.inv_break_productivity = '0'
+
+// UNION
+
+// SELECT 
+// 'ASIGNATION', 
+// COUNT(audit.id_break) 
+
+// FROM `MainAudit` as audit JOIN `InvBreak` as break 
+// ON 
+// audit.id_break = break.inv_break_id  
+
+// WHERE 
+// audit.id_break <> '0' 
+// AND 
+// break.inv_break_productivity = '1'
 
 
 export { SQLCdr };

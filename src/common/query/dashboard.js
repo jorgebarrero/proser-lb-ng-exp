10c3c33a521125d45c961d1a7914cd71e6fd44d2
jorgebarrero <jorgebarrero@gmail.com>
2019-04-24 09:45:53 -0400

    conexion-desconexion service updated

--- src/common/query/dashboard.js
+++ src/common/query/dashboard.js
@@ -1,3 +1,4 @@
+/*
 let SQLCdr = `
 SUM(cdr_call_received),
 SUM(cdr_call_abandoned),
@@ -14,54 +15,17 @@ sum(cdr_qlog_secs_at_wait)/SUM(cdr_call_received)
 
 let SQLAudit = `
 COUNT()
-<<<<<<< HEAD
-'
-=======
-
->>>>>>> 329a93600d47868ec929996e9e2d0118c7d74701
 SELECT 'LOGIN', COUNT(id_break), MIN(datetime_init), MAX(datetime_end) FROM 'MainAudit' WHERE id_break = '0'
 >>>>>>> 133303951ba4c5475a30bb8a825d17218c2e49b9
 
 // FROM `MainAudit`
 
-<<<<<<< HEAD
-// WHERE id_break = '0'
-=======
 SELECT 'AUXILIAR', COUNT(audit.id_break) FROM 'MainAudit' as audit JOIN 'InvBreak' as break ON audit.id_break = break.inv_break_id  WHERE audit.id_break <> '0' AND break.inv_break_productivity = '0'
->>>>>>> 133303951ba4c5475a30bb8a825d17218c2e49b9
 
 // UNION
 
-<<<<<<< HEAD
-// SELECT
-// 'AUXILIAR',
-// COUNT(audit.id_break)
-// FROM `MainAudit` as audit JOIN `InvBreak` as break
-// ON
-// audit.id_break = break.inv_break_id
-
-// WHERE
-// audit.id_break <> '0'
-// AND
-// break.inv_break_productivity = '0'
-
-// UNION
-
-// SELECT
-// 'ASIGNATION',
-// COUNT(audit.id_break)
-
-// FROM `MainAudit` as audit JOIN `InvBreak` as break
-// ON
-// audit.id_break = break.inv_break_id
-
-// WHERE
-// audit.id_break <> '0'
-// AND
-// break.inv_break_productivity = '1'
-=======
 SELECT 'ASIGNATION', COUNT(audit.id_break) FROM 'MainAudit' as audit JOIN 'InvBreak' as break ON audit.id_break = break.inv_break_id  WHERE audit.id_break <> '0' AND break.inv_break_productivity = '1'
->>>>>>> 133303951ba4c5475a30bb8a825d17218c2e49b9
 
 
 export { SQLCdr } `;
+*/
